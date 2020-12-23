832f60a60597f7121f7c59f14525c4eb25cc9c60
duilmarc <franco.chp@gmail.com>
2020-12-23 08:09:59 -0500

    creando la ruta generico para el uso del backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  BASE_URI : 'https://xse0s9zajf.execute-api.us-east-1.amazonaws.com/test'
 };
 
 /*
